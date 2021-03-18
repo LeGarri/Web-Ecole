b028a3d3cd3ae804d5b42d300a03f86e4ae29d2e
LeGarri <riobusor66@gmail.com>
2021-03-18 16:54:24 +0100

    change index.html to index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>Equipe BTSSN1 EC</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<header>
+	<div id="header">
+		<img id="header_image" src="images/Photo_de_Lattre.jpg" alt="photo de lattre">
+	</div>
+
+	<nav id="navlist">
+		<ul>
+			<li class="nav"><a href="index.html">Présentation</a></li>
+			<li class="nav"><a href="news.html">Actualités</a></li>
+			<li class="nav"><a href="register.html">Inscription</a></li>
+			<li class="nav"><a href="contact.html">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+<body>
+	<h1 id="text_presentation">Page présentation</h1>
+
+	<p id="text_btsn">Bienvenue sur le site de l'équipe BTSN1EC !</p>
+</body>
+
+<footer>
+	<p>Certains droits réservés</p>
+
+	<div id="footer_link">
+		<a href="index.html">Mentions légales - </a>
+		<a href="index.html">Plan du site</a>
+	</div>
+</footer>
